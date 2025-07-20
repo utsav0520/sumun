b7ee5be279138da83b9a872609b737a25b536eb6
Utsav Vachhani (uv) <uvachhani03@gmail.com>
2025-07-20 21:51:38 +0530

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 function TopWaves() {
   return (
     <svg
       viewBox="0 0 1440 320"
       xmlns="http://www.w3.org/2000/svg"
-      preserveAspectRatio="none" 
-      className="w-full h-[300px] sm:h-auto"
+      preserveAspectRatio="none"
+      className="w-full h-[200px] md:h-auto"
     >
       <path
         fill="#F30133"
@@ -15,10 +15,10 @@ function TopWaves() {
         fill="#05325B"
         d="M0,224L60,213.3C120,203,240,181,360,154.7C480,128,600,96,720,85.3C840,75,960,85,1080,112C1200,139,1320,181,1380,202.7L1440,224V320H0Z"
       />
-        <path
-          fill="#00A09E"
-          d="M0,288L48,272C96,256,192,224,288,213.3C384,203,480,213,576,218.7C672,224,768,224,864,202.7C960,181,1056,139,1152,117.3C1248,96,1344,96,1392,96L1440,96V320H0Z"
-        />
+      <path
+        fill="#00A09E"
+        d="M0,288L48,272C96,256,192,224,288,213.3C384,203,480,213,576,218.7C672,224,768,224,864,202.7C960,181,1056,139,1152,117.3C1248,96,1344,96,1392,96L1440,96V320H0Z"
+      />
     </svg>
   );
 }
@@ -29,7 +29,7 @@ function BottomWaves() {
       viewBox="0 0 1440 320"
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="none"
-      className="w-full h-[300px] sm:h-auto"
+      className="w-full h-[200px] md:h-auto"
     >
       <path
         fill="#00A09E"
@@ -47,30 +47,39 @@ function BottomWaves() {
   );
 }
 
-function Home() {
+const Home = () => {
   return (
-    <div className="relative text-black min-h-screen flex flex-col items-center justify-center overflow-hidden">
-      {/* Top Waves */}
-      <div className="absolute top-0 left-0 w-full rotate-180">
-        <TopWaves />
+    <>
+      <div className="relative text-black min-h-screen flex flex-col items-center justify-center overflow-hidden">
+        {/* Top Waves */}
+        <div className="absolute top-0 left-0 w-full rotate-180">
+          <TopWaves />
+        </div>
+
+        {/* Hero Text */}
+        <div className="z-10 mt-40 mb-40 text-center">
+          <h1 className="text-4xl sm:text-6xl font-bold leading-snug">
+            <span className="block">Voices that</span>
+            <span className="block text-[#F30133]">Leads</span>
+          </h1>
+        </div>
+
+        {/* Bottom Waves */}
+        <div className="absolute bottom-0 left-0 w-full">
+          <BottomWaves />
+        </div>
       </div>
 
-      {/* Hero Text */}
-      <div className="z-10 mt-40 mb-40 text-center">
-        <h1 className="text-4xl sm:text-6xl font-bold leading-snug">
-          <span className="block">Voices that</span>
-          <span className="block text-[#F30133]">Leads</span>
-        </h1>
+      <div className="h-[400px] text-black">
+        <h2 className="text-center text-3xl font-semibold mt-10">
+          Welcome to the Home Page
+        </h2>
+        <p className="text-center mt-4">
+          This is the starting point of our application. Explore the links in the navigation bar to learn more about us, our committees, and resources.
+        </p>
       </div>
-
-      {/* Bottom Waves */}
-      <div className="absolute bottom-0 left-0 w-full">
-        <BottomWaves />
-      </div>
-    </div>
+    </>
   );
 }
-
-
 
 export default Home;
