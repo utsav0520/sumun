aa2cb248dd47037fd89101b29901dc33fed68d99
Utsav Vachhani (uv) <uvachhani03@gmail.com>
2025-07-21 17:03:42 +0000

    home waves

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ function TopWaves() {
       viewBox="0 0 1440 320"
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="none"
-      className="w-full h-[200px] md:h-auto"
+      className="w-full h-[250px] md:h-auto"
     >
       <path
         fill="#F30133"
@@ -13,8 +13,9 @@ function TopWaves() {
       />
       <path
         fill="#05325B"
-        d="M0,224L60,213.3C120,203,240,181,360,154.7C480,128,600,96,720,85.3C840,75,960,85,1080,112C1200,139,1320,181,1380,202.7L1440,224V320H0Z"
+        d="M0,250L60,240C120,230,240,210,360,190C480,170,600,150,720,140C840,130,960,140,1080,160C1200,180,1320,210,1380,225L1440,240V320H0Z"
       />
+
       <path
         fill="#00A09E"
         d="M0,288L48,272C96,256,192,224,288,213.3C384,203,480,213,576,218.7C672,224,768,224,864,202.7C960,181,1056,139,1152,117.3C1248,96,1344,96,1392,96L1440,96V320H0Z"
@@ -29,7 +30,7 @@ function BottomWaves() {
       viewBox="0 0 1440 320"
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="none"
-      className="w-full h-[200px] md:h-auto"
+      className="w-full h-[250px] md:h-auto"
     >
       <path
         fill="#00A09E"
@@ -37,7 +38,7 @@ function BottomWaves() {
       />
       <path
         fill="#F30133"
-        d="M0,224L60,213.3C120,203,240,181,360,154.7C480,128,600,96,720,85.3C840,75,960,85,1080,112C1200,139,1320,181,1380,202.7L1440,224V320H0Z"
+        d="M0,250L60,240C120,230,240,210,360,190C480,170,600,150,720,140C840,130,960,140,1080,160C1200,180,1320,210,1380,225L1440,240V320H0Z"
       />
       <path
         fill="#05325B"
@@ -50,7 +51,7 @@ function BottomWaves() {
 const Home = () => {
   return (
     <>
-      <div className="relative text-black min-h-screen flex flex-col items-center justify-center overflow-hidden">
+      <div className="relative text-black min-h-screen  flex flex-col items-center justify-center overflow-hidden ">
         {/* Top Waves */}
         <div className="absolute top-0 left-0 w-full rotate-180">
           <TopWaves />
@@ -58,9 +59,9 @@ const Home = () => {
 
         {/* Hero Text */}
         <div className="z-10 mt-40 mb-40 text-center">
-          <h1 className="text-4xl sm:text-6xl font-bold leading-snug">
-            <span className="block">Voices that</span>
-            <span className="block text-[#F30133]">Leads</span>
+          <h1 className="text-4xl sm:text-8xl font-oswald-custom  leading-snug ">
+            <span className="block">VOICES THAT</span>
+            <span className="block text-[#F30133]">LEADS ...</span>
           </h1>
         </div>
 
@@ -70,16 +71,22 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="h-[400px] text-black">
+      <div className="h-[300px] text-black">
         <h2 className="text-center text-3xl font-semibold mt-10">
           Welcome to the Home Page
         </h2>
         <p className="text-center mt-4">
-          This is the starting point of our application. Explore the links in the navigation bar to learn more about us, our committees, and resources.
+          This is the starting point of our application. Explore the links in
+          the navigation bar to learn more about us, our committees, and
+          resources.
         </p>
+      </div>
+
+      <div className="bg-black test-white h-400">
+        utsav
       </div>
     </>
   );
-}
+};
 
 export default Home;
